0129ff69197ac142e2f7c093f9e21ba3aab86e04
Daruma.K <k.net.game01@gmail.com>
2025-07-23 17:39:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 // src/app/layout.tsx
 import './globals.css'; // Global styles
 import { Inter } from 'next/font/google'; // Google Font
-import NavBar from '../components/NavBar'; // Top Navigation Bar
+import NavBar from '@/components/NavBar'; // Top Navigation Bar
 import BottomNavbar from '@/components/BottomNavbar'; // Bottom Navigation Bar
-import Footer from '../components/Footer'; // Footer component (if applicable)
+import Footer from '@/components/Footer'; // Footer component (if applicable)
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* ใช้ Class สำหรับ Background Gradient ใหม่ที่เรากำหนดใน tailwind.config.js */}
-      <body className={`${inter.className} bg-my-new-gradient`}>
+      <body className={`${inter.className} bg-red-500`}>
         <NavBar /> {/* เมนูด้านบน */}
         <main className="flex-grow"> {/* เพิ่ม flex-grow เพื่อให้เนื้อหาหลักขยายเต็มที่ */}
           {children} {/* เนื้อหาของแต่ละ Page */}
