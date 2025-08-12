3c935ec5e75a3b151f95979189e25bf9a5261592
Daruma.K <k.net.game01@gmail.com>
2025-08-12 17:34:03 +0700

    Update page.tsx

--- app/buddha/page.tsx
+++ app/buddha/page.tsx
@@ -1,1 +1,9 @@
-buddha
+export default function AboutPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">พุทธศาสนา Microtronic</h1>
+      <p>หน้านี้จะเกี่ยวกับพุทธศาสนา และ คำสอนจากครูบาร์อาจารย์</p>
+      {/* เนื้อหา */}
+    </div>
+  );
+}
