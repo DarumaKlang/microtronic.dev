e5c01df275ee53897d10e5552d5e15aeca1c7286
Daruma.K <k.net.game01@gmail.com>
2025-08-15 08:34:24 +0700

    Update page.tsx

--- app/astrology/thursday/time/page.tsx
+++ app/astrology/thursday/time/page.tsx
@@ -27,7 +27,7 @@ const ThursdayAuspiciousTimes2 = () => {
   ];
 
   return (
-    <div className="container mx-auto p-4 pt-[100px] pb-[100px]">
+    <div className="container mx-auto p-4 pt-[80px] pb-[100px]">
       <h1 className="text-3xl font-bold mb-6">ฤกษ์ยามวันพฤหัสบดี (ชุด 2)</h1>
 
       <div className="mb-8">
