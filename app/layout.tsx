1af5114174d972935e16d3fbb47eaa13d78ff517
Daruma.K <k.net.game01@gmail.com>
2025-10-13 18:24:19 +0700

    update <SpeedInsights />, <Analytics />

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
             <body>
                 <NavBar />
                 {children}
+                <SpeedInsights />
+                <Analytics />
                 <BottomNavbar />
             </body>
         </html>
